2da7609f176b9d8c324cfaa028560beafb5a8310
vincent.capicotto <vincent.capicotto@hiptest.net>
2020-08-28 09:40:54 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-1",
+    BUCKET: "capic-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://dlkbmd1zk4.execute-api.eu-west-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_gb1CavyN2",
+    APP_CLIENT_ID: "6m7mqesqurvg5tsuh3icpbfqrq",
+    IDENTITY_POOL_ID: "eu-west-1:a2ebd275-d7ed-47e7-9967-b43e492fc5d2"
+  }
+};
